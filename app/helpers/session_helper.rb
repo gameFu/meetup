f75a48bbd9695467e0503a166450c75fb96cbda6
gameFu <223328084@qq.com>
2016-10-29 09:03:26 +0800

    fix current_user logic

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -14,18 +14,7 @@ module SessionHelper
 	end
 
   def current_user
-    # if request.content_type == 'application/json'
-    #   authenticate_with_http_token do |token, _options|
-    #         decode_token = (JWT.decode token, Rails.application.secrets['jwt_secret'], true, { :algorithm => Rails.application.secrets['jwt_algorithm'] })
-    #         @current_user = User.find_by(decode_token[0]['user']['user_id'])
-    #       end
-    #       @current_user
-    # else
-    #   if (user_id = session[:user_id])
-  	# 	    @current_user ||= User.find_by(id: user_id)
-    #   end
-    # end
-    (@current_user ||= User.find_by(id: request.env["X-USER-ID"])) unless (request.env["X-USER-ID"].nil?)
+    @current_user ||= User.find_by(id: request.env["X-USER-ID"])
   end
 
 end
